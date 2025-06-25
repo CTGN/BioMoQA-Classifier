3484ecd6ed525d9c8388a5509366e719103d4efe
Léandre Catogni <leandrecatogni@gmail.com>
2025-06-25 17:48:10 +0200

    update

--- scripts/launch_final.sh
+++ scripts/launch_final.sh
@@ -4,9 +4,6 @@ IFS=$'\n\t'
 
 # Models to iterate over
 MODELS=(
-  "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
-  "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext"
-  "FacebookAI/roberta-base"
   "dmis-lab/biobert-v1.1"
   "google-bert/bert-base-uncased"
 )
@@ -23,8 +20,7 @@ for opt_negs in "${NUM_OPT_NEGS[@]}"; do
   uv run src/data_pipeline/biomoqa/preprocess_biomoqa.py \
     -nf "${NUM_FOLDS}" \
     -nr "${NUM_RUNS}" \
-    -on "${opt_negs}"\
-    -t
+    -on "${opt_negs}"
 
   # repeat for each run
   for (( run=0; run<NUM_RUNS; run++ )); do
@@ -47,8 +43,7 @@ for opt_negs in "${NUM_OPT_NEGS[@]}"; do
             --n_trials 25 \
             --hpo_metric "eval_roc_auc" \
             -m "${model}" \
-            --loss "${loss}" \
-            -t
+            --loss "${loss}" 
 
           # Training with best HPO config
           uv run src/models/biomoqa/train.py \
@@ -60,8 +55,7 @@ for opt_negs in "${NUM_OPT_NEGS[@]}"; do
             --nb_opt_negs "${opt_negs}" \
             -bm "eval_roc_auc" \
             --gpu 2 \
-            --loss "${loss}" \
-            -t
+            --loss "${loss}" 
         done
 
         # Ensemble step for this fold/run
@@ -71,8 +65,7 @@ for opt_negs in "${NUM_OPT_NEGS[@]}"; do
           --fold "${fold}" \
           --run "${run}" \
           --nb_opt_negs "${opt_negs}" \
-          --loss "${loss}" \
-          -t
+          --loss "${loss}" 
       done
     done
   done
