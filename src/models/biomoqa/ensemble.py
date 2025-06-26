e10286d6923815d3b970622a09670ad006085d7e
Léandre Catogni <leandrecatogni@gmail.com>
2025-06-26 14:52:36 +0200

    Created Instantiation file

--- src/models/biomoqa/ensemble.py
+++ src/models/biomoqa/ensemble.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from typing import *
 import argparse
 import evaluate
 import matplotlib.pyplot as plt
---
+++ src/models/biomoqa/instantiation.py
@@ -0,0 +1,4 @@
+#Instantiation code :
+#Here I need to take a model's checkpoint and use it to evaluate a given text
+#I should then return the given text with the predicted score
+#For the code to be modular I shall give as argument the path to a model's weights
